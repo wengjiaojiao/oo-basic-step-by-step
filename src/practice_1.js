74a4ab3cefde50e9a0cabad2ccc40428f52dcebc
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-28 10:39:22 +0800

    write the practice and realize

---
+++ src/practice_1.js
@@ -0,0 +1,10 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.introduce = function(){
+    return "My name is " + this.name + ". I am " + this.age + " years old.";
+}
+
+module.exports = Person;
