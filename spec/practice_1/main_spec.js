62930408ae99d92ed2d0646d57c41c1d4c6db362
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-28 10:39:44 +0800

    write the spec file about practice_1

--- spec/practice_1/main_spec.js
+++ spec/practice_1/main_spec.js
@@ -1,19 +1,29 @@
 "use strict";
+
 var _ = require("lodash");
 var chai = require("chai");
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
+var Person = require("../../src/practice_1.js");
 
-describe("OO入门", function(){
-    it("写一个类-1", function(){
-        sinon.spy(console, 'log');
+describe("practiceOne", function(){
+    it("should tell us if the properties is defined",function() {
+        var person = new Person("Tom",21);
+        var name = "Tom";
+        var age = 21;
 
-        require("../../src/practice_1")
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '下面是部分学生的成绩详情\n沙龙逸\t语文124 数学148 英语137\n刘钰婷\t语文115 数学139 英语135\n韩林霖\t语文127 数学139 英语142\n平均成绩\t语文122 数学142 英语138';
-        
-        expect(expect_string).to.equal(result);
+        expect(person.name).equal(name);
+        expect(person.age).equal(age);
+
+    });
+
+    it("should print about name and age", function () {
+        var name = "Tom";
+        var age = 21;
+        var person = new Person(name,age);
+
+        expect(person.introduce()).equal("My name is Tom. I am 21 years old.");
     });
 });
