1b0dc146d83309cd56783b242d51667198c8cd92
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-28 13:18:57 +0800

    write the practiceThree and realize

---
+++ src/practice_3.js
@@ -0,0 +1,26 @@
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.introduce = function(){
+    return "My name is " + this.name + ". I am " + this.age + " years old.";
+}
+
+function Student(name, age, lesson) {
+    Person.call(name, age,this);
+    this.lesson = lesson;
+}
+
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function () {
+    return "I am a Student. I am at Class " + this.lesson + ".";
+}
+
+module.exports = {
+    Person:Person,
+    Student:Student
+}
